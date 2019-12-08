21169efd3163ed5a24616cc0e7bae93b08d06777
Guillaume Klaus <guillaumek@theodo.fr>
2019-12-08 21:36:25 +0100

    Fix XSS with DOMPurify

--- src/HtmlResult.tsx
+++ src/HtmlResult.tsx
@@ -1,14 +1,17 @@
 import React from "react";
+import DOMPurify from "dompurify";
 
 interface Props {
   htmlToInject?: string;
 }
 
 const HtmlResult = ({ htmlToInject = "" }: Props) => {
+  const sanitizedHtml = DOMPurify.sanitize(htmlToInject);
+
   return (
     <>
       <h2>Result</h2>
-      <div className="htmlResultContainer" dangerouslySetInnerHTML={{ __html: htmlToInject }} />
+      <div className="htmlResultContainer" dangerouslySetInnerHTML={{ __html: sanitizedHtml }} />
     </>
   );
 };
