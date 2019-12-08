1719df53f167fefb0fe54b8eaf92f3b58cc4fccd
Guillaume Klaus <guillaumek@theodo.fr>
2019-12-08 19:29:04 +0100

    Use EditorComponent in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import EditorConvertToHTML from "./EditorConvertToHTML";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Dojo Sécu - XSS</h1>
+      <br />
+      <EditorConvertToHTML />
     </div>
   );
-}
+};
 
 export default App;
